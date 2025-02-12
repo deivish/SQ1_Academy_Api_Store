1b7ef65da40e67f1f3a9050949d13d2d92d41b0e
deivis herrera <herrera.15cortes@gmail.com>
2025-02-12 00:34:35 -0500

    CartItem

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -1,1 +1,24 @@
-g
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    /** @use HasFactory<\Database\Factories\CartItemFactory> */
+    use HasFactory;
+
+    protected $fillable = ['cart_id', 'variant_id', 'quantity', 'unit_price'];
+
+    public function shoppingCart()
+    {
+        return $this->belongsTo(ShoppingCart::class, 'cart_id');
+    }
+
+    public function productVariant()
+    {
+        return $this->belongsTo(ProductVariant::class, 'variant_id');
+    }
+}
